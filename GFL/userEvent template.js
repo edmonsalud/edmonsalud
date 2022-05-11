c1f035a85f875ec463e5913effda2bebecc7c274
mayorjamesjayson <87059509+mayorjamesjayson@users.noreply.github.com>
2022-05-11 14:11:48 +0800

    created UE template

---
+++ GFL/userEvent template.js
@@ -0,0 +1,63 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record', 'N/file', 'N/search'],
+
+    function (record, file, search) {
+
+        /**
+         * Function definition to be triggered before record is loaded.
+         *
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {string} scriptContext.type - Trigger type
+         * @param {Form} scriptContext.form - Current form
+         * @Since 2015.2
+         */
+        function beforeLoad(scriptContext) {
+
+        }
+
+        /**
+         * Function definition to be triggered before record is loaded.
+         *
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type
+         * @Since 2015.2
+         */
+        function beforeSubmit(scriptContext) {
+
+            if (scriptContext.type == 'edit') {}
+        }
+
+        /**
+         * Function definition to be triggered before record is loaded.
+         *
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type
+         * @Since 2015.2
+         */
+        function afterSubmit(scriptContext) {
+
+
+            if (scriptContext.type == 'create') {}
+
+
+
+
+
+        }
+
+        return {
+            //     beforeLoad: beforeLoad,
+            beforeSubmit: beforeSubmit,
+            afterSubmit: afterSubmit
+        };
+
+    });
